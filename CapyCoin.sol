ffeb8488b5c2a44a53d9be55cae60c56935e0ca1
TheCashbara <161038842+TheCashbara@users.noreply.github.com>
2024-02-24 19:39:02 +0100

    Update CapyCoin.sol

--- CapyCoin.sol
+++ CapyCoin.sol
@@ -20,7 +20,6 @@ import "./IPinkAntiBot.sol";
 
 
 contract CapyCoin is ERC20, Ownable {
-    address public immutable _capylikBurnWallet;
 
     /// @notice The contract implementing tax calculations.
     ITaxHandler public taxHandler;
@@ -36,26 +35,21 @@ contract CapyCoin is ERC20, Ownable {
 
     constructor(
         address taxHandlerAddress,
-        address capylikBurnWallet,
         address pinkAntiBot_
     ) ERC20("CapyCoin", "CAPYCOIN") Ownable(_msgSender()) {
-        require(taxHandlerAddress != address(0) && capylikBurnWallet != address(0), "taxHandlerAddress and capylikBurnWallet can't be the zero address.");
+        require(taxHandlerAddress != address(0), "taxHandlerAddress can't be the zero address.");
         
-        _capylikBurnWallet = capylikBurnWallet;
         taxHandler = ITaxHandler(taxHandlerAddress);
 
         uint256 totalSupply = 1000000000 * (10**decimals());
-        uint256 halfTotalSupply = totalSupply / 2;
         // Mint initial supply to the contract owner
-        _mint(_msgSender(), halfTotalSupply);
-        // Mint burn event supply to capylik burn wallet
-        _mint(capylikBurnWallet, halfTotalSupply);
+        _mint(_msgSender(), totalSupply);
 
         // Create an instance of the PinkAntiBot variable from the provided address
         pinkAntiBot = IPinkAntiBot(pinkAntiBot_);
         // Register the deployer to be the token owner with PinkAntiBot. You can
         // later change the token owner in the PinkAntiBot contract
-       pinkAntiBot.setTokenOwner(_msgSender());
+        pinkAntiBot.setTokenOwner(_msgSender());
     }
 
     function _update(
@@ -66,9 +60,7 @@ contract CapyCoin is ERC20, Ownable {
         require(
             from != address(0) ||
                 (from == address(0) &&
-                    (to == address(owner()) || to == _capylikBurnWallet)),
-            "From can't be the zero address"
-        );
+                    to == address(owner())), "From can't be the zero address");
 
         if (from == address(0)) {
             super._update(from, to, amount);
